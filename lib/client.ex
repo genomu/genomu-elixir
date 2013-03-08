0dafddee2266c7ba39f27233277d272507d00144
Yurii Rashkovskii <yrashk@spawngrid.com>
2013-03-08 12:12:43 -0800

    Add Genomu.Client.begin and commit functions

--- lib/client.ex
+++ lib/client.ex
@@ -2,4 +2,12 @@ defmodule Genomu.Client do
   def connect(options) do
     :supervisor.start_child(Genomu.Client.Sup.Connections, [options])
   end
+
+  def begin(conn, _options // []) do
+    Genomu.Client.Connection.start_channel(conn)
+  end
+
+  def commit(channel, _options // []) do
+    Genomu.Client.Channel.commit(channel)
+  end
 end
