d6cab6d141b7803918498b6e1c08520f1c263312
Yurii Rashkovskii <yrashk@spawngrid.com>
2013-03-09 12:49:29 -0800

    Improved API

--- lib/client.ex
+++ lib/client.ex
@@ -1,4 +1,11 @@
 defmodule Genomu.Client do
+
+  defmacro __using__(_) do
+    quote do
+      require Genomu.Client
+    end
+  end
+
   def connect(options) do
     :supervisor.start_child(Genomu.Client.Sup.Connections, [options])
   end
@@ -7,7 +14,29 @@ defmodule Genomu.Client do
     Genomu.Client.Connection.start_channel(conn)
   end
 
-  def commit(channel, _options // []) do
-    Genomu.Client.Channel.commit(channel)
+  def transaction(conn, f, options // []) when is_function(f, 1) do
+    {:ok, ch} = begin(conn, options)
+    try do
+      result = f.(ch)
+      :ok = commit(ch)
+      result
+    catch _, _ ->
+      # TODO: rollback
+      :ok
+    end
   end
+
+  defmacro execute(conn, ch, [do: body]) do
+    quote do
+      Genomu.Client.transaction(unquote(conn), fn(unquote(ch)) -> unquote(body) end)
+    end
+  end
+
+  defdelegate [get(channel, addr, operation, options),
+               get(channel, addr, operation),
+               set(channel, addr, operation, options),
+               set(channel, addr, operation),
+               apply(channel, addr, operation, options),
+               apply(channel, addr, operation),
+               commit(channel)], to: Genomu.Client.Channel
 end
